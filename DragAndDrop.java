95011f0070da8ce738e25af511252b8ea8e068b5
Saranya Easwaran <Saranya.amr@gmail.com>
2019-05-22 19:39:39 +0530

    second commit

--- DragAndDrop.java
+++ DragAndDrop.java
@@ -2,15 +2,21 @@ package allTests;
 
 import org.testng.annotations.Test;
 import utility.BaseTest;
+import utility.FrameworkConstants;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 
 public class DragAndDrop extends BaseTest{
 	
 	@Test
-	public void test() throws InterruptedException {		
+	public void test() throws InterruptedException {
+		System.setProperty(FrameworkConstants.CHROME_DRIVER_KEY,FrameworkConstants.CHROME_DRIVER_PATH);
+		WebDriver driver = new ChromeDriver();		
 		driver.findElement(By.xpath("//a[contains(text(),'Drag and Drop')]")).click();
 		WebElement elementA = driver.findElement(By.cssSelector("div#column-a"));
 		System.out.println(elementA.getAttribute("id"));
